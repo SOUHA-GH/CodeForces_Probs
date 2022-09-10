2b717a6af5857be3cf5581ae3824647f83fed408
Souha Ghabri <107365513+SOUHA-GH@users.noreply.github.com>
2022-09-10 10:53:41 +0200

    Create 231A- Team.c

---
+++ A/231A- Team.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{int a,d,b,i,n,c;
+scanf("%d",&n);
+d=0;
+for(i==0;i<n;i++){
+    scanf("%d ",&a);
+    scanf("%d ",&b);
+    scanf("%d",&c);
+if(a+b+c>=2)
+    d=d+1;}
+printf("%d",d);
+}
